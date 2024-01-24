2293e17bdf8dae1eb13fef4cef0a4978d44eac07
Vitaliy Gromov <gromov.vitaliy03@gmail.com>
2024-01-24 21:22:13 +0300

    remove unnessesary methods from Filterable trait

--- app/Filters/Traits/Filterable.php
+++ app/Filters/Traits/Filterable.php
@@ -4,54 +4,11 @@ declare(strict_types=1);
 
 namespace App\Filters\Traits;
 
-use App\Filters\Core\AbstractFilter;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Query\Builder;
-use Illuminate\Support\Arr;
 
 trait Filterable
 {
-    public static function getFilterClassName(): string
-    {
-        $modelName = class_basename(static::class);
-
-        return 'App\Filters\\'. $modelName.'Filter';
-    }
-
-    public static function createInstanceOfFilterClass(Builder $builder): object
-    {
-        $filter = new (self::getFilterClassName())($builder);
-
-        self::setFilterTable($filter);
-
-        return $filter;
-    }
-
-    public static function setFilterTable(AbstractFilter $filter): void
-    {
-        $filter->from = self::getTableName();
-    }
-
-    public static function getTableName(): string
-    {
-        return ((new self)->getTable());
-    }
-
-    public static function getFilterClassMethods(AbstractFilter $filter): array
-    {
-        $reflection = new \ReflectionClass($filter);
-
-        $methods = [];
-
-        foreach ($reflection->getMethods() as $method){
-            if ($method->class === self::getFilterClassName()){
-                $methods[] = $method->name;
-            }
-        }
-
-        return $methods;
-    }
-
     public static function filter(array $requestData)
     {
         /** @var EloquentBuilder $query */
@@ -70,5 +27,17 @@ trait Filterable
         }
 
         return $query;
+    }
+
+    public static function createInstanceOfFilterClass(Builder $builder): object
+    {
+        return new (self::getFilterClassName())($builder);
+    }
+
+    public static function getFilterClassName(): string
+    {
+        $modelName = class_basename(static::class);
+
+        return 'App\Filters\\'. $modelName.'Filter';
     }
 }
